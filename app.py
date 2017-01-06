6548837bf25b725c5207da7484e83aa7148c7fe6
Xiang <3120100496@zju.edu.cn>
2017-01-06 21:02:19 +0000

    error 404

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ app = Flask(__name__,template_folder='templates')
 # Register the controllers
 app.register_blueprint(controllers.main)
 
-
-# Debug
-# if __name__ == '__main__':
-#     app.run(host=config.env['host'], port=config.env['port'], debug=True)
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error404.html'), 404
